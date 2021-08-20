fb040afb55fddfa296c50f44cfd04c07277373ee
Gerald Haboc <77391983+colesitecode@users.noreply.github.com>
2021-08-20 10:08:44 +0800

    styleee

--- pages/Services.js
+++ pages/Services.js
@@ -54,6 +54,8 @@ const Border1 = styled.div`
   width:0;
   height:0;
   border-top:2px solid #e21f36;
+  border-image: ;
+
   position:absolute;
   background:none;
   transition:all .5s ease-in-out;
