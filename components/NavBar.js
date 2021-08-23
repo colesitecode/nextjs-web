01942419be9e7ba719a4295db5b56c21f84858f8
Gerald Haboc <77391983+colesitecode@users.noreply.github.com>
2021-08-23 10:43:58 +0800

    change navbar

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,35 +4,6 @@ import { Button, Span1, Span2, Span3, Span4 } from '../styles/GlobalStyle'
 import Image from 'next/image'
 import logo from '../components/images/logo.png'
 
-const Nav = styled.div`
-    width: 100%;
-    height: 80px;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 25px;
-    padding-right: 25px;
-    background-color: rgba(0, 2, 25, 0.33);
-`
-
-const Logo = styled.div`
-    padding-left: 70px;
-`
-
-const NavMenu = styled.div`
-    display: flex;
-`
-
-const NavLink = styled.h3`
-    padding-left: 15px;
-    font-size: 20px;
-
-    a{
-        color: #ffffff;
-    }
-`
-const TopHeadBTN = styled.div``
-
 function NavBar() {
     return (
         <Nav>
@@ -78,5 +49,37 @@ function NavBar() {
         </Nav>
     )
 }
+
+
+// ---------------STYLE-----------------
+const Nav = styled.div`
+    width: 100%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 25px;
+    padding-right: 25px;
+    background-color: rgba(0, 2, 25, 0.33);
+`
+
+const Logo = styled.div`
+    padding-left: 70px;
+`
+
+const NavMenu = styled.div`
+    display: flex;
+`
+
+const NavLink = styled.h3`
+    padding-left: 15px;
+    font-size: 20px;
+
+    a{
+        color: #ffffff;
+    }
+`
+const TopHeadBTN = styled.div``
+
 
 export default NavBar
